361913eae594781bd02c08785d7a8369ea86eeb0
n-ono <>
2020-10-28 07:39:21 +0900

    first commit

---
+++ src/test/java/railway/fare_calculation/FareCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package railway.fare_calculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FareCalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
